33fee5d991d29265c9c4237aad24abf9b1abcc10
Rizki Putra <rputrar@assistxenterprise.ai>
2026-01-15 11:30:19 +0700

    feat: implement new web design and update pipeline logic

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,12 @@
-FROM python:3.9-slim
-
-ENV PYTHONDONTWRITEBYTECODE=1
-ENV PYTHONUNBUFFERED=1
+FROM python:3.9-slim-buster
 
 WORKDIR /app
 
-RUN pip install --no-cache-dir --upgrade pip setuptools \
-    && pip install --no-cache-dir "jaraco.context>=6.1.0" \
-    && find /usr/local/lib/python3.9/site-packages -name "jaraco.context-5.3.0.dist-info" -exec rm -rf {} +
-
-COPY app/requirements.txt .
-
+COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY . .
+COPY app/ .
 
 EXPOSE 5000
 
-CMD ["python", "app/app.py"]
+CMD ["python", "app.py"]
--- app/app.py
+++ app/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
-@app.route("/")  # Pastikan menggunakan .route BUKAN .main
-def hello():
-    return "Hello, ini aplikasi Flask yang berjalan di Kubernetes!"
+# Route untuk file statis seperti CSS/Gambar
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(os.path.join(root_dir, 'app', 'static'), filename)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)  # nosec
+@app.route('/')
+def hello_world():
+    message = "Selamat datang di K8s Explorer Anda!"
+    subtitle = "Aplikasi ini berjalan di Kubernetes!"
+    return render_template('index.html', message=message, subtitle=subtitle)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
